5f0cc069919c3ee294b555250c96230cd33533f4
Gary Bishop <gary.bishop.unc@gmail.com>
2025-03-17 16:54:28 -0400

    Add more pages.

--- scripts/nws.js
+++ scripts/nws.js
@@ -1,1 +1,12 @@
+function conditions_to_title() {
+  const current = document.querySelector("#current_conditions-summary");
+  if (!current) {
+    console.log("not found");
+    return;
+  }
+  const T = current.querySelector("p.myforecast-current-lrg").innerHTML;
+  const text = current.querySelector("p.myforecast-current").innerHTML;
+  document.title = `${T} ${text} NWS`;
+}
 setInterval(() => location.reload(), 30 * 60 * 1000);
+window.addEventListener("load", conditions_to_title);
--- scripts/pws.js
+++ scripts/pws.js
@@ -1,1 +1,11 @@
+// reload the page every 10 minutes
 setInterval(() => location.reload(), 10 * 60 * 1000);
+function temperature_to_title() {
+  document.title = document.querySelector(
+    "div.main-temp > lib-display-unit > span > span",
+  ).innerText;
+}
+window.addEventListener("load", () => {
+  temperature_to_title();
+  setInterval(temperature_to_title, 10 * 1000);
+});
